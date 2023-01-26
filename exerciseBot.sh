7b6583f72a0b0b1ce8281e0db71e386a5763f439
FerntheFlerm2 <ferin.vonreich@colorado.edu>
2023-01-26 10:48:21 -0700

    refactor to use lazy cron

--- exerciseBot.sh
+++ exerciseBot.sh
@@ -3,15 +3,23 @@
 #note that log redirected to /var/log/exerciseBotBackup.log
 
 #got to this directory (so cron uses correct enviroment)
-cd /home/gigabyte/.customCommands/cron/exerciseBot
+cd /home/gigabyte/.customCommands/cron/exerciseBot || exit 2
+
+#check if already ran today - for lazy cron
+todaysDate="$(echo "$(date)" | awk '{print $1, $3, $2}')"
+read -r firstLine < lastDayRan.ignore
+if [ "$todaysDate" == "$firstLine" ]; then
+    exit 0
+else
+    echo "$todaysDate" > lastDayRan.ignore
+fi
 
 # check if every other day
 # https://serverfault.com/questions/204265/how-to-configure-cron-job-to-run-every-2-days-at-11pm
-todaysDate="$(echo "$(date)" | awk '{print $1, $3, $2}')"
 if [ -f altday.ignore ]; then
-  echo "$todaysDate - altday, did nothing"
+  notify-send "$todaysDate - altday, did nothing"
   rm altday.ignore
-  exit
+  exit 0
 fi
 touch altday.ignore
 
@@ -37,3 +45,14 @@ END_MAIL
 
 #rm image
 rm $picName
+
+#notify user
+notify-send "Exercise Bot Sent"
+
+#For Crontab:
+# redirect to its own file: https://askubuntu.com/questions/56683/where-is-the-cron-crontab-log
+#0 6 * * * ~/.customCommands/cron/exerciseBot/exerciseBot.sh > /var/log/exerciseBotBackup.log 2>&1
+
+#For lazy cron
+#/tmp/LazyCron_logs
+
